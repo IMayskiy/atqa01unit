0a5114b47a765068c460bb5782cbc941624211c6
Igor R <igrasputin@yandex.ru>
2023-06-22 22:55:37 +0300

    Initial commit

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,12 +1,33 @@
 package ru.netology.unit;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CashbackHackServiceTest {
 
-class CashbackHackServiceTest {
-
-    @Test
-    void remain() {
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 00;
+        assertEquals(expected, actual);
     }
+    @org.junit.Test
+    public void testRemainCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+    @org.junit.Test
+    public void testRemainCashback2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+
 }
